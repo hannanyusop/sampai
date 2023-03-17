7df27d3e89e515a8062ab468fbbdf50e4fb88616
Hannan <hannan@devstack.com.my>
2023-03-17 17:24:34 +0800

    password

--- app/Domains/Auth/Http/Controllers/Frontend/Auth/LoginController.php
+++ app/Domains/Auth/Http/Controllers/Frontend/Auth/LoginController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Rules\Captcha;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use LangleyFoxall\LaravelNISTPasswordRules\PasswordRules;
 
 /**
  * Class LoginController.
@@ -59,7 +58,7 @@ class LoginController extends Controller
     {
         $request->validate([
             $this->username() => ['required', 'max:255', 'string'],
-            'password' => array_merge(['max:100'], PasswordRules::login()),
+            'password' => array_merge(['max:100', 'required']),
             'g-recaptcha-response' => ['required_if:captcha_status,true', new Captcha],
         ], [
             'g-recaptcha-response.required_if' => __('validation.required', ['attribute' => 'captcha']),
