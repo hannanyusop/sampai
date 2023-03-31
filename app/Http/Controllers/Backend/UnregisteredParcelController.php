332ff21841e00f7dce65d4ae4cb4d3ad0b303d46
Hannan <hannan@devstack.com.my>
2023-04-01 02:14:50 +0800

    feat : add migration

---
+++ app/Http/Controllers/Backend/UnregisteredParcelController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\UnregisteredParcel;
+use Illuminate\Http\Request;
+
+class UnregisteredParcelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UnregisteredParcel  $unregisteredParcel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UnregisteredParcel $unregisteredParcel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UnregisteredParcel  $unregisteredParcel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UnregisteredParcel $unregisteredParcel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UnregisteredParcel  $unregisteredParcel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UnregisteredParcel $unregisteredParcel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UnregisteredParcel  $unregisteredParcel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UnregisteredParcel $unregisteredParcel)
+    {
+        //
+    }
+}
