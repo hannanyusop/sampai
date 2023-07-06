ffc2bceaf565cd0170daafe496bbd9010e7c30f3
hannany <nan_s96@yahoo.com>
2023-07-06 23:23:49 +0800

    feat : parcel show

--- resources/views/backend/parcel/index.blade.php
+++ resources/views/backend/parcel/index.blade.php
@@ -8,6 +8,14 @@
 
 @section('content')
 
+    <div class="nk-block-head nk-block-head-sm">
+        <div class="nk-block-between g-3">
+            <div class="nk-block-head-content">
+                <h3 class="nk-block-title page-title">Parcel List / Form Response</h3>
+            </div>
+        </div>
+    </div>
+
     <div class="nk-block nk-block-lg">
         <div class="card card-bordered card-preview">
             <div class="card-inner">
