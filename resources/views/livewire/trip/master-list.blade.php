6ec5da5019adfccf87ba9e17971bb50db2120e63
hannanyusop <nan_s96@yahoo.com>
2024-05-15 20:42:44 +0800

    fix : add loading action

--- resources/views/livewire/trip/master-list.blade.php
+++ resources/views/livewire/trip/master-list.blade.php
@@ -16,12 +16,21 @@
 {{--                        <a href="#" wire:click="exportWhatsappBot()" class="btn btn-success d-none d-sm-inline-flex"><em class="icon ni ni-download-cloud"></em><span>Whatsapp Bot Export</span></a>--}}
 
                         <div class="drodown m-2">
-                            <button href="#" class="dropdown-toggle btn btn-info" data-toggle="dropdown"><em class="icon ni ni-download-cloud"></em><span>Whatsapp Bot Export</span></button>
+                            <button href="#" class="dropdown-toggle btn btn-info"
+                                    data-toggle="dropdown"><em class="icon ni ni-download-cloud"></em>
+                                <b wire:loading.remove>Whatsapp Bot Export</b>
+                                <b wire:loading>
+                                    Exporting...
+                                </b>
+                            </button>
                             <div class="dropdown-menu dropdown-menu-right">
                                 <ul class="link-list-opt no-bdr">
                                     <a ref="#" wire:click="exportWhatsappBot()">
                                         <em class="icon ni ni-chevron-right"></em>
                                         <span>All</span>
+                                        <p wire:loading>
+                                            Exporting...
+                                        </p>
                                     </a>
                                 </ul>
                                 @foreach($trip_batch->trips as $trip)
@@ -29,6 +38,9 @@
                                         <a href="#" wire:click="exportWhatsappBot({{ $trip->id }})">
                                             <em class="icon ni ni-chevron-right"></em>
                                             <span>Only For {{ $trip->destination->code }}</span>
+                                            <p wire:loading>
+                                                Exporting...
+                                            </p>
                                         </a>
                                     </ul>
                                 @endforeach
